627c2b3f3f7db2440962e3d3d51939e6be6d3ff4
yjshin2 <yjshin2@enliple.com>
2022-12-27 19:12:34 +0900

    add .env router setup

---
+++ src/components/Router.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import  {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+/*인증 여부에 따라 달라질 부분 구현*/
+export default function AppRouter ()  {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <Router>
+            <Switch>
+                {isLoggedIn ? (
+                <>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+                </>
+                )    :    (
+                <Route exact path="/">
+                    <Auth/>
+                </Route>
+                )}
+            </Switch>
+        </Router>
+    );
+}
+
+/*
+    <></>
+    Fragment가 무엇이냐면, 많은 요소들을 render하고 싶을때 사용함.
+    그렇다고 div는 넣기 싫고 span도 넣기 싫고 할때 씀.
+
+* */
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBjaG6-OyYJhF9YpLrNUuWQ97HUeU-CI28",
-    authDomain: "nwitter-463ac.firebaseapp.com",
-    projectId: "nwitter-463ac",
-    storageBucket: "nwitter-463ac.appspot.com",
-    messagingSenderId: "454761753798",
-    appId: "1:454761753798:web:a0fd2a22c4040c17a5bbc7"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
