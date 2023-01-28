44ffa594472c10913a63ef1f73c996db899f2d81
shin-yujin-mac <in1006sh@gmail.com>
2023-01-28 17:22:23 +0900

    #4.1 Preview Images part One and Two

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -15,6 +15,7 @@ import Nweet from "../components/Nweet";
 const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState("");
 
     useEffect(() => {
         const q = query(
@@ -45,6 +46,23 @@ const Home = ({userObj}) => {
         setNweet(value);
     };
 
+    const onFileChange = (e) => {
+        const {
+            target : {files},
+        } = e;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                currentTarget: {result},
+            } = finishedEvent;
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    };
+
+    const onClearAttachment = () => setAttachment(null)
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -53,7 +71,14 @@ const Home = ({userObj}) => {
                        onChange={onChange}
                        placeholder="what's on you maid?"
                        maxLength={120}/>
+                <input type="file" accept="image/*" onChange={onFileChange}/>
                 <input type="submit" value="Nweet"/>
+                {attachment &&
+                    <div>
+                        <img src={attachment} width="50px" height="50px"/>
+                        <button onClick={onClearAttachment }>Clear</button>
+                    </div>
+                }
             </form>
             <div>
                 {nweets.map(nweet => (
